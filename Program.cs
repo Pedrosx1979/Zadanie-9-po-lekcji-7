f84711d1dcb0de1e0a764df3765caef4da4c2f3e
Piotr Szydlak <pedrosx@o2.pl>
2023-12-05 09:56:08 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Wprowadź temperaturę ....");
+int temp = Int32.Parse(Console.ReadLine());
+
+if (temp < 0)
+{
+    Console.WriteLine("Cholernie Piździ ....");
+}
+else if (temp >= 0 && temp <= 10 )
+{
+    Console.WriteLine("Zimno ....");
+}
+else if (temp >= 10 && temp  >= 20) 
+{
+    Console.WriteLine("Chłodno ....");
+}
+else if (temp >= 20 && temp  >= 30) 
+{
+    Console.WriteLine("Jest w sam raz ....");
+}
+else if (temp >= 30 && temp  >= 40) 
+{
+    Console.WriteLine("Zaczyna być gorąco ....");
+}
+else if (temp >= 40 ) 
+{
+    Console.WriteLine("A weź wyprowadzam się na ALASKĘ ....");
+}
